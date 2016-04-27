cbbc972383183304ff4f91108874bc7e3a39abae
HoraceChan <chenhouren2008@gmail.com>
2016-04-28 01:38:35 +0800

    修复：消费记录为倒序输出

--- src/main/java/com/horacework/repository/UsermoneyRepository.java
+++ src/main/java/com/horacework/repository/UsermoneyRepository.java
@@ -11,9 +11,9 @@ import java.util.List;
 @Repository
 public interface UsermoneyRepository extends JpaRepository<UsermoneyEntity,String>{
 
-    @Query(value = "select p from UsermoneyEntity p where p.userId=:userId and p.currentTime=(SELECT max(q.currentTime)FROM UsermoneyEntity q WHERE q.userId=:userId)")
+    @Query(value = "select p from UsermoneyEntity p where p.userId=:userId and p.currentTime=(SELECT max(q.currentTime)FROM UsermoneyEntity q WHERE q.userId=:userId) ")
     UsermoneyEntity findUserMoneyLastLogById(@Param("userId") String userId);
 
-    @Query(value = "select p from UsermoneyEntity p where p.userId=:userId")
+    @Query(value = "select p from UsermoneyEntity p where p.userId=:userId  ORDER BY p.currentTime DESC")
     List<UsermoneyEntity> findUserMoneyAllLogById(@Param("userId") String userId);
 }
